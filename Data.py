ffb0337e06829e63b9d2a81e3600d45b27c5efc0
Toxic Dee Modder <90701056+ToxicDeeModder1@users.noreply.github.com>
2021-11-09 18:48:55 +0200

    Update Data.py

--- Data.py
+++ Data.py
@@ -10,7 +10,7 @@ Welcome to {}
 
 You can use me to manage channels with tons of features. Use below buttons to learn more !
 
-By @StarkBots
+By @TheBotsWorldChannel
     """
 
     # Home Button
@@ -20,13 +20,13 @@ By @StarkBots
 
     # Rest Buttons
     buttons = [
-        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/StarkBots/7")],
+        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/TheBotsWorldChannel")],
         [
             InlineKeyboardButton("How to Use ❔", callback_data="help"),
             InlineKeyboardButton("🎪 About 🎪", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
-        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/StarkBotsChat")],
+        [InlineKeyboardButton("♥ Developer♥", url="https://t.me/ToxicDeeModderr")],
+        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/NarutoSupportChat")],
     ]
 
     # Help Message
@@ -50,13 +50,13 @@ Alternative Commands
     ABOUT = """
 **About This Bot** 
 
-A telegram channel automation bot by @StarkBots
+A telegram channel automation bot by @TheBotsWorldChannel
 
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/ChannelBot)
+
 
 Framework : [Pyrogram](docs.pyrogram.org)
 
 Language : [Python](www.python.org)
 
-Developer : @StarkProgrammer
+Developer : @ToxicDeeModderr
     """
